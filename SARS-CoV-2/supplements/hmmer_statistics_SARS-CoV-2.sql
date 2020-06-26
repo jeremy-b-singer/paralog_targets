18e4e16654bcfbe0e6e3a80f5b2f05ec15fd9b6f
Jeremy B Singer <jsinger@rcn.com>
2020-06-26 07:49:36 -0400

    hmmer_statistics data from table for SARS-CoV-2.

---
+++ SARS-CoV-2/supplements/hmmer_statistics_SARS-CoV-2.sql
@@ -0,0 +1,2 @@
+\copy (select * from hmmer_statistics h WHERE h.tax_id = 2697049 ) to 'C:/Users/Jeremy-satellite/Documents/RBIF120/paralog_targets/SARS-CoV-2/supplements/hmmer_statistics_SARS-CoV-2.txt' CSV HEADER delimiter '	'
+
